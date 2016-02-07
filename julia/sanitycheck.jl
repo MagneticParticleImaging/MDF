297606e8fb94d4c1f674aaa85dd7ea107952c997
Martin Hofmann <mhofmann@uke.de>
2016-02-07 20:11:38 +0100

    added checks for element type of datasets.

--- julia/sanitycheck.jl
+++ julia/sanitycheck.jl
@@ -71,22 +71,28 @@ end
 function _hasCorrectType1_0(fid)
   result = true
   groups = Dict{ASCIIString, Vector{Tuple{ASCIIString,Type}}}(
-  "/" => [("version",ASCIIString), ("uuid",ASCIIString), ("date",ASCIIString)],
-  "/scanner/" => [("facility",ASCIIString), ("operator",ASCIIString), ("manufacturer",ASCIIString), ("model",ASCIIString), ("topology",ASCIIString)],
-  "/aquisition/" => [("numFrames",Int64), ("framePeriod",Float64), ("numPatches",Int64), ("gradient",Float64), ("time",ASCIIString)],
-  "/aquisition/drivefield/" => [("numChannels",Int64), ("strength",Float64), ("baseFrequency",Float64), ("divider",Int64), ("period",Float64), ("averages",Int64), ("repetitionTime",Float64), ("fieldOfView",Float64), ("fieldOfViewCenter",Float64)])
+  "/" => [("version",Char), ("uuid",Char), ("date",Char)],
+  "/study/" => [("name",Char), ("experiment",Char), ("description",Char), ("subject",Char), ("reference",Int64), ("simulation", Int64)],
+  "/tracer/" => [("name",Char), ("batch",Char), ("vendor",Char), ("volume",Float64), ("concentration",Float64), ("time", Char)],
+  "/scanner/" => [("facility",Char), ("operator",Char), ("manufacturer",Char), ("model",Char), ("topology",Char)],
+  "/aquisition/" => [("numFrames",Int64), ("framePeriod",Float64), ("numPatches",Int64), ("gradient",Float64), ("time",Char)],
+  "/aquisition/drivefield/" => [("numChannels",Int64), ("strength",Float64), ("baseFrequency",Float64), ("divider",Int64), ("period",Float64), ("averages",Int64), ("repetitionTime",Float64), ("fieldOfView",Float64), ("fieldOfViewCenter",Float64)],
+  "/aquisition/receiver/" => [("numChannels",Int64), ("bandwidth",Float64), ("numSamplingPoints",Int64), ("frequencies",Float64), ("transferFunctions",Float64)],
+  "/measurement/" => [("dataFD",Any), ("dataTD", Any)])
 
   for group in keys(groups)
     hasgroup = exists(fid, group)
     if hasgroup
       g = fid[group]
-      for (dataset,datatype) in groups[group]
+      for (dataset,eldatatype) in groups[group]
         hasdataset = exists(g, dataset)
         if hasdataset
-	  test = g[dataset]
-	  correcttype = HDF5.hdf5_to_julia(test)==datatype
-	  result = result & correcttype
-	  !correcttype && warn("HDF5 dataset $dataset in HDF5 group $group has type $(HDF5.hdf5_to_julia(test)) but should have $datatype.")
+	  dset = g[dataset]
+	  #println(dset, ndims(dset))
+	  dsettype = eltype(HDF5.hdf5_to_julia(dset))
+	  hascorrecttype = dsettype<:eldatatype
+	  result = result & hascorrecttype
+	  !hascorrecttype && warn("$dset has element type $dsettype but should have $eldatatype.")
 	end
       end
     end
