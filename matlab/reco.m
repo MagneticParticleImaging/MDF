fc9e52057474328520d55ec2716dab7ce647a0fc
Gael Bringout <gael.bringout@gmail.com>
2016-03-29 19:24:29 +0200

    corrected the used svd
    fix #33 by using the 'econ' mode of matlab's svd in order to get the
    equivalent of the "thin" svd of julia. Then extracted the diagonal of
    the sigma matrix to get the singular values.

--- matlab/reco.m
+++ matlab/reco.m
@@ -3,9 +3,6 @@
 clear all
 close all
 
-addpath(genpath(fullfile('.','regu')))
-addpath(genpath(fullfile('.','ourFunctions')))
-
 %% 2. Download measurement and systemMatrix from http://media.tuhh.de/ibi/mdf/
 
 filenameSM = 'systemMatrix.h5';
@@ -15,8 +12,6 @@ urlwrite('http://media.tuhh.de/ibi/mdf/systemMatrix.h5',filenameSM)
 urlwrite('http://media.tuhh.de/ibi/mdf/measurement_5.h5',filenameMeas)
 
 %% 3. Loading the data
-disp('2. Load the data'); tic
-
 % For the System matrix (later named SM)
 % to obtain infos on the file, use the command: infoSM = h5info(filename_SM);
 % or read the format documentation
@@ -32,7 +27,6 @@ S = squeeze(S(1,:,:,:) + 1i*S(2,:,:,:));
 % note that these data contain 500 measurements
 u = h5read(filenameMeas, '/measurement/dataFD');
 u = squeeze(u(1,:,:,:) + 1i*u(2,:,:,:));
-toc
 
 %% 4. Pre-process - Remove the frequencies which are lower than 30 kHz, as they are unreliable due to the anologue filter in the scanner
 
@@ -50,8 +44,6 @@ S_truncated = reshape(S_truncated, size(S_truncated,1), size(S_truncated,2)*size
 u_truncated = reshape(u_truncated, size(u_truncated,1)*size(u_truncated,2), size(u_truncated,3));
 
 %% 6. Averaged the measurement used for the reconstruction over all temporal frames
-disp('6. Post-processing: average the measurements'); tic
-
 u_mean_truncated = mean(u_truncated,2);
 
 %% 7. Make two simple reconstructions
@@ -61,8 +53,9 @@ c_normReguArt = kaczmarzReg(S_truncated(:,:),...
                         1,1*10^-6,0,1,1);
 
 % and an regularized pseudoinverse approach
-[U,Sigma,V] = csvd(S_truncated(:,:).');
-c_pseudoInverse = pseudoinverse(U,Sigma,V,u_mean_truncated,5*10^3,1,1);
+[U,Sigma,V] = svd(S_truncated(:,:).','econ');
+Sigma2 = diag(Sigma);
+c_pseudoInverse = pseudoinverse(U,Sigma2,V,u_mean_truncated,5*10^3,1,1);
 
 %% 8. Display an image
 % read the original size of an image
